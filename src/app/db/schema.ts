f02a3b8eec43a795554683ca1a1207b80fc54b01
Fazil <faziltirtana@tutanota.com>
2024-06-15 16:01:35 +0700

    fix: schema

--- src/app/db/schema.ts
+++ src/app/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, serial, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const Ticket = pgTable("tickets", {
   ticket_id: uuid("id").defaultRandom().notNull().primaryKey(),
@@ -14,27 +21,26 @@ export const Ticket = pgTable("tickets", {
     "medium"
   ),
   last_updated: timestamp("last_updated").defaultNow().notNull(),
-  messages: .....
 });
 
-export const MockFeels=pgTable('mockFeels', {
-  id:serial('id').primaryKey(),
-  jsonMockResp:text('jsonMockResp').notNull(),
-  feel:varchar('feel').notNull(),
-  feelDesc:varchar('feelDesc').notNull(),
-  language:varchar('language').notNull(),
-  createdBy:varchar('createdBy').notNull(),
-  createdAt:varchar('createdAt'),
-  mockId:varchar('mockId').notNull()
-})
+export const MockFeels = pgTable("mockFeels", {
+  id: serial("id").primaryKey(),
+  jsonMockResp: text("jsonMockResp").notNull(),
+  feel: varchar("feel").notNull(),
+  feelDesc: varchar("feelDesc").notNull(),
+  language: varchar("language").notNull(),
+  createdBy: varchar("createdBy").notNull(),
+  createdAt: varchar("createdAt"),
+  mockId: varchar("mockId").notNull(),
+});
 
-export const UserAnswer=pgTable('userAnswer', {
-  id:serial('id').primaryKey(),
-  mockIdRef:varchar('mockId').notNull(),
-  question:varchar('question').notNull(),
-  userAns:text('userAns'),
-  feedback:text('feedback'),
-  language:varchar('language').notNull(),
-  userEmail:varchar('userEmail'),
-  createdAt:varchar('createdAt')
-})
+export const UserAnswer = pgTable("userAnswer", {
+  id: serial("id").primaryKey(),
+  mockIdRef: varchar("mockId").notNull(),
+  question: varchar("question").notNull(),
+  userAns: text("userAns"),
+  feedback: text("feedback"),
+  language: varchar("language").notNull(),
+  userEmail: varchar("userEmail"),
+  createdAt: varchar("createdAt"),
+});
